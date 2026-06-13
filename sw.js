afe46b3d068aedd636a7be3ad75414da6105df3e
Poshyvailo Volodymyr <v.poshyvailo@m2e.team>
2026-06-13 20:15:13 +0300

    Added icon

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ const CACHE_NAME = 'math-pwa-v1';
 const ASSETS = [
     './index.html',
     './manifest.json',
+    './icon-512.png',
     'https://cdn.tailwindcss.com',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
     'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.js'
