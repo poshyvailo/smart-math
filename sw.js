6af2c767129cb5679d24a92e937b4bf0dbb1f1b4
Poshyvailo Volodymyr <v.poshyvailo@m2e.team>
2026-06-13 20:52:55 +0300

    Fixes SW

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'math-pwa-v1';
+const CACHE_NAME = 'math-pwa-v2';
 const ASSETS = [
     './index.html',
     './manifest.json',
     './icon-512.png',
-    'https://cdn.tailwindcss.com',
+    'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
-    'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.js'
+    'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.prod.js'
 ];
 
 self.addEventListener('install', (e) => {
